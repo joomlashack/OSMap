ecbaf06e1234d4af873e0f844949dd19b75a564f
Bill Tomczak <bill@joomlashack.com>
2021-09-10 12:49:42 -0700

    Convert sitemaps modal to bootstrap (Joomla v3)

--- src/admin/form/fields/sitemaps.php
+++ src/admin/form/fields/sitemaps.php
@@ -22,114 +22,145 @@
  * along with OSMap.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use Joomla\CMS\Version;
+use Alledia\OSMap\Factory;
+use Joomla\CMS\Form\FormField;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\Utilities\ArrayHelper;
 
 defined('_JEXEC') or die();
 
-jimport('joomla.form.field');
+require_once 'TraitOsmapField.php';
 
-/**
- * Supports a modal sitemap picker.
- *
- * @package             OSMap
- * @subpackage          com_osmap
- * @since               2.0
- */
-class OsmapFormFieldSitemaps extends JFormField
+class OsmapFormFieldSitemaps extends FormField
 {
+    use TraitOsmapField;
+
     /**
-     * The field type.
-     *
-     * @var    string
+     * @inheritdoc
      */
     protected $type = 'Sitemaps';
 
     /**
-     * Method to get a list of options for a sitemaps list input.
-     *
-     * @return    array        An array of JHtml options.
+     * @var string
      */
+    protected static $scripts = [];
+
     protected function getInput()
     {
-        // Initialise variables.
-        $db  = JFactory::getDBO();
-        $doc = JFactory::getDocument();
+        HTMLHelper::_('jquery.framework');
+        HTMLHelper::_('script', 'system/modal-fields.js', ['version' => 'auto', 'relative' => true]);
 
-        HTMLHelper::_('bootstrap.renderModal', 'a.btn-modal');
+        $function = 'osmapSelectSitemap_' . $this->id;
 
-        // Get the name of the linked chart
-        if ($this->value) {
-            $query = $db->getQuery(true)
-                ->select('name')
-                ->from('#__osmap_sitemaps')
-                ->where('id = ' . (int)$this->value);
-            $name = $db->setQuery($query)->loadResult();
-        } else {
-            $name = '';
+        // Build the script.
+        if (empty(static::$scripts[$this->id])) {
+            $script = <<<JSCRIPT
+window.{$function} = function(id, name) {
+    window.processModalSelect('Sitemap', '{$this->id}', id, name);
+}
+JSCRIPT;
+
+            Factory::getDocument()->addScriptDeclaration($script);
+
+            static::$scripts[$this->id] = true;
         }
 
-        if (empty($name)) {
-            $name = JText::_('COM_OSMAP_SELECT_AN_SITEMAP');
+        $app     = Factory::getApplication();
+        $context = sprintf('%s.%s', $app->input->getCmd('option'), $app->input->getCmd('view'));
+
+        // Build the link
+        $linkQuery = [
+            'option'   => 'com_osmap',
+            'view'     => 'sitemaps',
+            'layout'   => 'modal',
+            'tmpl'     => 'component',
+            'context'  => $context,
+            'function' => $function
+        ];
+
+        $link = 'index.php?' . htmlspecialchars(http_build_query($linkQuery));
+
+        // Get sitemap title if one selected
+        $value = (int)$this->value ?: '';
+        if ($value) {
+            $db = Factory::getDbo();
+            $db->setQuery(
+                $db->getQuery(true)
+                    ->select('name')
+                    ->from('#__osmap_sitemaps')
+                    ->where('id = ' . (int)$this->value)
+            );
+            $title = $db->loadResult();
         }
 
-
-
-        $link = 'index.php?option=com_osmap&amp;view=sitemaps&amp;layout=modal&amp;tmpl=component&amp;function=jSelectSitemap_' . $this->id;
-
-        if (Version::MAJOR_VERSION < 4) {
-            JHtml:_('behavior.framework');
-            // Load the modal behavior.
-            JHtml::_('behavior.modal', 'a.btn-modal');
-
-            $doc->addScriptDeclaration(
-                "function jSelectSitemap_" . $this->id . "(id, name, object) {
-                   $('" . $this->id . "_id').value = id;
-                   $('" . $this->id . "_name').value = name;
-                   SqueezeBox.close();
-              }"
-            );
-
-            $html = '<span class="input-append">' . "\n";
-            $html .= '<input class="input-medium" type="text" id="' . $this->id . '_name" value="' . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '" disabled="disabled" />';
-            $html .= '<a class="btn-modal btn" title="' . JText::_('COM_OSMAP_CHANGE_SITEMAP') . '"  href="' . $link . '" rel="{handler: \'iframe\', size: {x: 800, y: 450}}"><i class="icon-file"></i> ' . JText::_('COM_OSMAP_CHANGE_SITEMAP_BUTTON') . '</a>' . "\n";
-            $html .= '</span>' . "\n";
-            $html .= '<input type="hidden" id="' . $this->id . '_id" name="' . $this->name . '" value="' . (int) $this->value . '" />';
-        } else {
-            HTMLHelper::_('jquery.framework');
-
-            $modalId = 'ModalSelectSitemapModal_' . $this->id;
-
-            $html = '<div class="input-group">' . "\n";
-            $html .= '<input class="form-control" type="text" id="' . $this->id . '_name" value="' . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '" disabled="disabled" />';
-            $html .= '<a class="btn btn-secondary btn-modal" data-bs-toggle="modal" data-bs-target="#' . $modalId . '" title="' . JText::_('COM_OSMAP_CHANGE_SITEMAP') . '"  href="' . $link . '"><i class="icon-file"></i> ' . JText::_('COM_OSMAP_CHANGE_SITEMAP_BUTTON') . '</a>' . "\n";
-            $html .= '</div>' . "\n";
-            $html .= '<input type="hidden" id="' . $this->id . '_id" name="' . $this->name . '" value="' . (int) $this->value . '" />';
-
-            $html .= HTMLHelper::_(
-                'bootstrap.renderModal',
-                $modalId,
-                array(
-                    'title'       => JText::_('COM_OSMAP_CHANGE_SITEMAP'),
-                    'url'         => $link,
-                    'height'      => '400px',
-                    'width'       => '800px',
-                    'bodyHeight'  => 70,
-                    'modalWidth'  => 80,
-                    'footer'      => '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">'
-                        . JText::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</button>',
-                )
-            );
-
-            $doc->addScriptDeclaration(
-                "
-                function jSelectSitemap_" . $this->id . "(id, name, object) {
-                   $('#" . $this->id . "_id').val(id);
-                   $('#" . $this->id . "_name').val(name);
-                   $('[data-bs-dismiss=\"modal\"]').trigger('click');
-              }"
-            );
+        if (empty($title)) {
+            $title = Text::_('COM_OSMAP_OPTION_SELECT_SITEMAP');
         }
+
+        $title   = htmlspecialchars($title, ENT_QUOTES);
+        $modalId = 'ModalSelectSitemap_' . $this->id;
+
+        // Begin field output
+        $html = '<span class="input-append">';
+
+        // Display the read-only name field
+        $html .= sprintf(
+            '<input %s/>',
+            ArrayHelper::toString([
+                'type'     => 'text',
+                'id'       => $this->id . '_name',
+                'value'    => $title,
+                'class'    => 'input-medium',
+                'disabled' => 'disabled',
+                'size'     => 35
+            ])
+        );
+
+        // Create read-only ID field
+        $attribs = [
+            'type'          => 'hidden',
+            'id'            => $this->id . '_id',
+            'name'          => $this->name,
+            'value'         => $value,
+            'data-required' => (int)$this->required
+        ];
+        if ($this->required) {
+            $attribs['class'] = 'class="required modal-value';
+        }
+        $html .= sprintf('<input  %s/>', ArrayHelper::toString($attribs));
+
+        // Select button
+        $html .= HTMLHelper::_(
+            'link',
+            '#' . $modalId,
+            '<span class="icon-list" aria-hidden="true"></span> ' . Text::_('JSELECT'),
+            [
+                'class'       => 'btn btn-primary hasTooltip',
+                'id'          => $this->id . '_change',
+                'data-toggle' => 'modal',
+                'role'        => 'button',
+                'title'       => HTMLHelper::tooltipText(Text::_('COM_OSMAP_OPTION_SELECT_SITEMAP')),
+            ]
+        );
+
+        // Modal Sitemap window
+        $html .= HTMLHelper::_(
+            'bootstrap.renderModal',
+            $modalId,
+            [
+                'title'      => Text::_('COM_OSMAP_OPTION_SELECT_SITEMAP'),
+                'url'        => $link,
+                'height'     => '400px',
+                'width'      => '800px',
+                'bodyHeight' => '70',
+                'modalWidth' => '80',
+                'footer'     => sprintf(
+                    '<a role="button" class="btn" data-dismiss="modal" aria-hidden="true">%s</a>',
+                    Text::_('JLIB_HTML_BEHAVIOR_CLOSE')
+                ),
+            ]
+        );
 
         return $html;
     }
