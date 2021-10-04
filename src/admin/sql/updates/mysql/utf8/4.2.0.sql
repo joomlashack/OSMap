a420aaad997d7f86daa968b26ab3f6cdeeb8ed3b
Bill Tomczak <bill@joomlashack.com>
2021-10-04 10:29:54 -0700

    Update for corrected index

--- src/admin/sql/updates/mysql/utf8/4.2.0.sql
+++ src/admin/sql/updates/mysql/utf8/4.2.0.sql
@@ -3,7 +3,7 @@
 ALTER TABLE `#__osmap_sitemaps`
     DROP INDEX `default`;
 ALTER TABLE `#__osmap_sitemaps`
-    ADD INDEX `idx_default` (`is_default`);
+    ADD INDEX `idx_default` (`is_default` ASC, `id` ASC);
 
 -- ============================================================================
 -- Add the column "format" to the item settings table
